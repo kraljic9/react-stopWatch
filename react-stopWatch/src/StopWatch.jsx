8639ec028b8d3b6d168025bce99efebff66ff152
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-12-26 11:15:44 +0100

    Added html skeleton, contains a container for the stop watch, a paragraph containing time and a div containing all the buttons

--- react-stopWatch/src/StopWatch.jsx
+++ react-stopWatch/src/StopWatch.jsx
@@ -1,5 +1,16 @@
 function StopWatch() {
-  return <></>;
+  return (
+    <>
+      <div className="stop-watch-container">
+        <p className="time">00:00:00</p>
+        <div className="controls">
+          <button className="start-button">Start</button>
+          <button className="stop-button">Stop</button>
+          <button className="reset-button">Reset</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default StopWatch;
