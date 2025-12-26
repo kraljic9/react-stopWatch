b5a9918cc2937512ce11d209734d901342ce4de9
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-12-26 11:31:53 +0100

    Added functions to start, stop and reset timer, added useEffect containing a interval that will setElapsedTime every 10 miliseconds

--- react-stopWatch/src/StopWatch.jsx
+++ react-stopWatch/src/StopWatch.jsx
@@ -6,14 +6,46 @@ function StopWatch() {
   const intervalId = useRef(null);
   const startTime = useRef(0);
 
+  useEffect(() => {
+    intervalId.current = setInterval(() => {
+      setElapsedTime(Date.now() - startTime.current);
+    }, 10);
+
+    return () => {
+      clearInterval(intervalId.current);
+    };
+  }, [isRunning]);
+
+  function start() {
+    setIsRunning(true);
+    startTime.current = Date.now() - elapsedTime;
+    console.log(startTime.current);
+  }
+
+  function stop() {
+    setIsRunning(false);
+  }
+
+  function reset() {
+    setIsRunning(false);
+    setElapsedTime(0);
+  }
+  function displayTime() {}
+
   return (
     <>
       <div className="stop-watch-container">
         <p className="time">00:00:00</p>
         <div className="controls">
-          <button className="start-button">Start</button>
-          <button className="stop-button">Stop</button>
-          <button className="reset-button">Reset</button>
+          <button className="start-button" onClick={start}>
+            Start
+          </button>
+          <button className="stop-button" onClick={stop}>
+            Stop
+          </button>
+          <button className="reset-button" onClick={reset}>
+            Reset
+          </button>
         </div>
       </div>
     </>
